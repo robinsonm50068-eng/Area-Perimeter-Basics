e0aa9c7e39ba410d8dcf0efd9cce50581118251e
robinsonm50068-eng <robinsonm50068@masseyhigh.school.nz>
2026-08-10 10:44:47 +1200

    name thing

---
+++ P_03_input_output.py
@@ -0,0 +1,12 @@
+ # ask the user for their name
+username = input("What is your name?")
+
+ # ask the user for their favourite number (integer)
+fav_num = int("What is your favourite number?")
+
+ # Double, halve adn square the users favourite number
+
+ #Greet the user
+
+ # Output the results of doubling, halving and
+ # squaring their favourite integer
