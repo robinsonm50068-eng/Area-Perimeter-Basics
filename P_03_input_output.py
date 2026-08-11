f8906dfaa0770fb4e51980a0c4bfb8925b6ea177
robinsonm50068-eng <robinsonm50068@masseyhigh.school.nz>
2026-08-12 10:59:29 +1200

    times stuff

--- P_03_input_output.py
+++ P_03_input_output.py
@@ -1,12 +1,19 @@
- # ask the user for their name
-username = input("What is your name?")
+# ask the user for their name
+username = input("What is your name? ")
 
- # ask the user for their favourite number (integer)
-fav_num = int("What is your favourite number?")
+# ask the user for their favourite number (integer)
+fav_num = int(input("What is your favourite number?"))
 
- # Double, halve adn square the users favourite number
+# Double, halve adn square the users favourite number
+double = fav_num * 2
+halve = fav_num / 2
+square = fav_num * fav_num
 
- #Greet the user
+# Greet the user
+print(f"Hi {username}, your favourite number is {fav_num}")
+print(f"Double {fav_num} is {double}, ")
+print(f"Halve {fav_num} is {halve}, ")
+print(f"Square {fav_num} is {square}, ")
 
- # Output the results of doubling, halving and
- # squaring their favourite integer
+# Output the results of doubling, halving and
+# squaring their favourite integer
