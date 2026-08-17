b76b8d485866420c2e264e5c73dc413c42d42a48
robinsonm50068-eng <robinsonm50068@masseyhigh.school.nz>
2026-08-18 10:46:08 +1200

    dint do something same as previous code commited

--- P_04__get_width.py
+++ P_04__get_width.py
@@ -5,8 +5,10 @@ error = "Please enter a number that is more than zero\n"
 while True:
 
     try:
+        # ask the user for a number
         width = float(input("width: "))
 
+        # check that the number is more than zero
         if width > 0:
             break
         else:
