1ff7e1ae52186f29cbeeac0d078341519e4d295e
robinsonm50068-eng <robinsonm50068@masseyhigh.school.nz>
2026-08-18 10:43:28 +1200

    enter number code vid 4

---
+++ P_04__get_width.py
@@ -0,0 +1,16 @@
+# Ask user for width and loop until they
+# enter a number that is more than zero
+
+error = "Please enter a number that is more than zero\n"
+while True:
+
+    try:
+        width = float(input("width: "))
+
+        if width > 0:
+            break
+        else:
+            print(error)
+
+    except ValueError:
+        print(error)
