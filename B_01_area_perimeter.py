ef42c9ae136a507c4e247a6ce2af99c834b15553
robinsonm50068-eng <robinsonm50068@masseyhigh.school.nz>
2026-08-27 10:32:25 +1200

    Add files via upload

--- B_01_area_perimeter.py
+++ B_01_area_perimeter.py
@@ -24,9 +24,20 @@ keep_going = ""
 while keep_going == "":
 
     # Get width and height
+    width = num_check("width: ")
+    height = num_check("height: ")
 
     # Calculate area / perimeter
+    area = width * height
+    perimeter = 2 * (width + height)
 
     # Display output
+    print()
+    print(f"Perimeter: {perimeter} units")
+    print(f"Area: {area} square units")
 
     # Ask user if they want to keep going
+    keep_going = input("Press enter to keep going or any key to quit. ")
+    print()
+
+print("Thank you for using the area / perimeter calculator")
