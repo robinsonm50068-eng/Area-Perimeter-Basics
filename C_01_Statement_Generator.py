a3a364181cdf819857f73656f2bf1b696650439a
robinsonm50068-eng <robinsonm50068@masseyhigh.school.nz>
2026-09-02 13:28:37 +1200

    Add files via upload

---
+++ C_01_Statement_Generator.py
@@ -0,0 +1,8 @@
+# functions go here 1 usage
+
+def statement_generator(statement, decoration):
+    print(f"\n{decoration * 5} {statement} {decoration}")
+
+
+#Main routine goes here
+statement_generator("Instructions", "-")
